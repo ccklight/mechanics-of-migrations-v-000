bc5943ba8426294a358dae12195b618d3ac4c860
Cheryl K <cherylkelson@icloud.com>
2019-01-22 19:40:52 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -34,3 +34,15 @@ def change
   create_table :artists do |t|
   end
 end
+
+
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
